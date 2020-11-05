5e7015ccfca3232323cb741da1fe0d375d08cb2f
Kris Foster <me@krismade.me>
2020-11-05 11:54:29 +0000

    Various: * Load testing script * SQL to configure the pluggable Oracle DB * Dockerfile for openjdk container * Makefile to run everything * Markdown file for hsoting performance graphs across all runtimes

---
+++ c1.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+ARTIFACT=petclinic-jpa
+MAINCLASS=org.springframework.samples.petclinic.PetClinicApplication
+VERSION=0.0.1-SNAPSHOT
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+rm -rf target
+mkdir -p target/native-image
+
+echo "Packaging $ARTIFACT with Maven"
+mvn -ntp package -DskipTests > target/native-image/output.txt
+
+JAR="$ARTIFACT-$VERSION.jar"
+rm -f $ARTIFACT
+echo "Unpacking $JAR"
+cd target/native-image
+jar -xvf ../$JAR >/dev/null 2>&1
+cp -R META-INF BOOT-INF/classes
+
+LIBPATH=`find BOOT-INF/lib | tr '\n' ':'`
+CP=BOOT-INF/classes:$LIBPATH
